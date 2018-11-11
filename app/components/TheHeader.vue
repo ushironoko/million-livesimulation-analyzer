cd2dc58e7803595b338f50a80a681de726a6e3cd
ushironoko <apple19940820@yahoo.co.jp>
2018-11-11 14:19:57 +0900

    タイトルかえた

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <el-menu mode="horizontal" :router="true" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
     <el-menu-item index="1" style="pointer-events:none;">
-      Live Skill View
+      ミリシタカード API
     </el-menu-item>
   </el-menu>
 </template>
