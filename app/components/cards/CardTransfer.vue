a28d1949791fc0281896f38b81c7afc693e6107b
ushironoko <apple19940820@yahoo.co.jp>
2018-12-08 01:45:59 +0900

    スキル全被りまで

--- app/components/cards/CardTransfer.vue
+++ app/components/cards/CardTransfer.vue
@@ -14,17 +14,42 @@
         label:'カード名'
       }"
       :titles="['カード一覧','編成']"
-      :data="cardData.filter(data => data.name.toLowerCase())">
+      :data="cardData.filter(data => data.name.toLowerCase())"
+      >
     </el-transfer>
+    <el-card>
+      <span>{{ `${lcmData}秒後にスキルの発動が被ります`}}</span>
+
+    </el-card>
   </section>
 </template>
 
 <script>
+import math from 'mathjs'
+
 export default {
   props: ['cardData', 'loading'],
   data() {
     return {
       selection: []
+    }
+  },
+  computed: {
+    lcmData() {
+      if(this.selection.length > 1) {
+        const selections = this.selection
+        const filteredList = this.cardData
+        .filter(function(data) {
+          return selections.includes(data.name)
+        }).map(x => {
+          return x.skill[0].interval
+        })
+
+        const lcmResult = math.lcm(...filteredList)
+        return lcmResult
+      } else {
+        return 0
+      }
     }
   }
 }
