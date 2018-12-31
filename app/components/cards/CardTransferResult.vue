ae80e9c4a83b1522336a662ff33a4e15ee4d1853
ushironoko <apple19940820@yahoo.co.jp>
2018-12-31 22:36:20 +0900

    テーブルレイアウト調整

--- app/components/cards/CardTransferResult.vue
+++ app/components/cards/CardTransferResult.vue
@@ -4,14 +4,14 @@
       <span>{{ `${lcmData}秒後にすべてのスキル発動が被ります`}}</span>
     </div>
     <hr>
-    <el-table :data="filteredList" max-height="800" fixed>
-      <el-table-column label="カード名" fixed prop="name">
+    <el-table :data="filteredList" height="300">
+      <el-table-column label="カード名" prop="name">
       </el-table-column>
-      <el-table-column label="スキル名" fixed prop="skillName">
+      <el-table-column label="スキル名" prop="skillName">
       </el-table-column>
-      <el-table-column label="インターバル" fixed prop="skill[0].interval">
+      <el-table-column label="インターバル" prop="skill[0].interval">
       </el-table-column>
-      <el-table-column label="スキル秒数" fixed prop="skill[0].duration">
+      <el-table-column label="スキル秒数" prop="skill[0].duration">
       </el-table-column>
     </el-table>
   </el-card>
