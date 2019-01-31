e9a67bcb2015e09efa10acd75c824a5a7fbddb47
ushironoko <apple19940820@yahoo.co.jp>
2019-02-01 01:24:19 +0900

    レイアウト調整、表示データ変更

--- app/components/livesimulates/LiveSimulateResult.vue
+++ app/components/livesimulates/LiveSimulateResult.vue
@@ -5,12 +5,10 @@
     <el-tabs tab-position="left">
       <el-tab-pane label="結果">
         <div>
-          <span>スコア</span>
+          <span>結果</span>
         </div>
-        <el-table :data="liveSimulationData" max-height="960" style="max-width: 800px;">
-          <el-table-column label="最大スコア" prop="ScoreInfo.Total.Ideal">
-          </el-table-column>
-          <el-table-column label="最小スコア" prop="ScoreInfo.Total.Min">
+        <el-table :data="liveSimulationData" max-height="960">
+          <el-table-column label="最大" prop="ScoreInfo.Total.Ideal">
           </el-table-column>
           <el-table-column label="0.1%" prop="ScoreInfo.Total.p[0]">
           </el-table-column>
@@ -22,19 +20,29 @@
       </el-tab-pane>
       <el-tab-pane label="編成">
         <div>
-          <span>編成</span>
+          <span>編成
+          </span>
         </div>
-        <el-table :data="selectedCardList" target-order="push" max-height="960" style="max-width: 800px;">
+        <el-table :data="selectedCardList" target-order="push" max-height="960">
           <el-table-column label="カード">
               <template slot-scope="scope">
-                <a :href="createImgUrl(scope.row.id)" target="_blunk"><img :src="scope.row.resourceId" style="max-width: 100px;"/></a>
+                <a :href="createImgUrl(scope.row.id)" target="_blunk"><img :src="scope.row.resourceId" style="max-width: 100px; width: 100%;"/></a>
               </template>
           </el-table-column>
-          <el-table-column label="発動間隔" prop="skill[0].interval">
+          <el-table-column label="ボーカル値">
+              <template slot-scope="scope">
+                <span>{{ scope.row.vocalMaxAwakened }}<br>{{ "(" + `${scope.row.vocalMaxAwakened + scope.row.vocalMasterBonus}` + ")" }}</span>
+              </template>
           </el-table-column>
-          <el-table-column label="発動秒数" prop="skill[0].duration">
+          <el-table-column label="ビジュアル値">
+              <template slot-scope="scope">
+                <span>{{ scope.row.visualMaxAwakened }}<br>{{ "(" + `${scope.row.visualMaxAwakened + scope.row.visualMasterBonus}` + ")" }}</span>
+              </template>
           </el-table-column>
-          <el-table-column label="上昇率" prop="skill[0].value">
+          <el-table-column label="ダンス値">
+              <template slot-scope="scope">
+                <span>{{ scope.row.danceMaxAwakened }}<br>{{ "(" + `${scope.row.danceMaxAwakened + scope.row.danceMasterBonus}` + ")" }}</span>
+              </template>
           </el-table-column>
         </el-table>
       </el-tab-pane>
@@ -60,3 +68,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.el-table {
+  max-width: 800px;
+  width: 100%;
+}
+</style>
