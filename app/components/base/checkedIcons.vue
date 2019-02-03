fab60457f6365beff8e153dffd407994e1e03d5e
ushironoko <apple19940820@yahoo.co.jp>
2019-02-03 22:14:49 +0900

    フィルタをチェックボックスに変更、画像選択後回し

---
+++ app/components/base/checkedIcons.vue
@@ -0,0 +1,28 @@
+<template>
+  <span @click="changeChecked" >
+    <img :id="cardId" :src="imageUrl" style="max-width: 100px;" :class="{ checked:isChecked }">
+  </span>
+</template>
+
+<script>
+export default {
+  props:['imageUrl','cardId'],
+  data() {
+    return {
+      isChecked: false
+    }
+  },
+  methods: {
+    changeChecked(){
+      this.checked = !this.checked
+    }
+  }
+}
+</script>
+
+<style>
+.checked {
+   border:solid 2px #9eceff;
+   border-radius: 0.5em;
+}
+</style>
