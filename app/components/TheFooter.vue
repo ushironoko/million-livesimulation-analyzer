78412d6fc5f3fe33fc990a0617d6ee6f02b98fa2
ushironoko <apple19940820@yahoo.co.jp>
2019-02-17 02:19:00 +0900

    footerにindexつけた

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <el-menu mode="horizontal" class="footer" background-color="#01adb9">
-    <el-menu-item>
+    <el-menu-item index="4">
       <a href="https://api.matsurihi.me/docs" target="_blank" rel="noopener">All card information is from matsurihi.me.</a>
     </el-menu-item>
-    <el-menu-item>
+    <el-menu-item index="4">
       <a href="https://api.megmeg.work/doc/" target="_blank" rel="noopener">music & live score calc API is from megmeg.work .</a>
     </el-menu-item>
-    <el-menu-item>
+    <el-menu-item index="4">
       <a href="https://twitter.com/sirootosan" target="_blank" rel="noopener">twitter</a>
     </el-menu-item>
   </el-menu>
