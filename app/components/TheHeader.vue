e2b31dbf4b20e5c4d89e7b9113feb8c931b7288c
ushironoko <apple19940820@yahoo.co.jp>
2019-02-17 20:03:30 +0900

    テーマカラー変更

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-menu class="header" mode="horizontal" :router="true" background-color="#01adb9" text-color="#fff" active-text-color="#fff">
+  <el-menu class="header" mode="horizontal" :router="true" background-color="#24292D" text-color="#fff" active-text-color="#fff">
     <el-menu-item index="1" route="/" >
       ミリシタスコア計算機
     </el-menu-item>
