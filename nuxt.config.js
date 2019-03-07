a328448bf3249d4e8e94eddcba5264528521b0d1
ushironoko <apple19940820@yahoo.co.jp>
2019-03-07 19:46:10 +0900

    WIP

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,8 @@ module.exports = {
     short_name: 'MSC',
     title: 'ミリシタスコア計算機',
     'og:title': 'ミリシタスコア計算機',
-    description: 'ミリシタのライブスコア計算機。',
-    'og:description': 'ミリシタのライブスコア計算機。',
+    description: 'ミリシタのスコアを計算できるシンプルな計算機。',
+    'og:description': 'ミリシタのスコアを計算できるシンプルな計算機。',
     lang: 'ja',
     theme_color: '#24292D',
     background_color: '#e6ecf0'
@@ -28,12 +28,12 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'ミリシタのライブスコア計算機。' },
+      { hid: 'description', name: 'description', content: 'ミリシタのスコアを計算できるシンプルな計算機。' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'ミリシタスコア計算機' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: 'https://millionlive-simple-score-calc.me/' },
       { hid: 'og:title', property: 'og:title', content: 'ミリシタスコア計算機' },
-      { hid: 'og:description', property: 'og:description', content: 'ミリシタのライブスコア計算機。'}
+      { hid: 'og:description', property: 'og:description', content: 'ミリシタのスコアを計算できるシンプルな計算機。'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
@@ -67,10 +67,7 @@ module.exports = {
   modules: [
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
-    '@nuxtjs/dotenv',
-    ['@nuxtjs/google-analytics', {
-      id: process.env.GA_UA_ID
-    }]
+    '@nuxtjs/dotenv'
   ],
 
   /*
@@ -79,8 +76,7 @@ module.exports = {
   env: {
     FIREBASE_V1_CARDS_SSR_ENDPOINT:process.env.FIREBASE_V1_CARDS_SSR_ENDPOINT,
     MEGMEGWORK_API_ENDPOINT:process.env.MEGMEGWORK_API_ENDPOINT,
-    MATSURIHIME_ICON:process.env.MATSURIHIME_ICON,
-    GA_UA_ID:process.env.GA_UA_ID,
+    MATSURIHIME_ICON:process.env.MATSURIHIME_ICON
   },
 
   /*
