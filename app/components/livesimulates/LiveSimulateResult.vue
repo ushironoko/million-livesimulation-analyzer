3119fe635588f2f47c011055f1b4b9444ce936bc
ushironoko <apple19940820@yahoo.co.jp>
2019-05-01 18:47:25 +0900

    リファクタ

--- app/components/livesimulates/LiveSimulateResult.vue
+++ app/components/livesimulates/LiveSimulateResult.vue
@@ -65,9 +65,9 @@
       </span>
     </el-container>
 
-    <el-table border :data="simuResult" max-height="960">
-      <el-table-column label="楽曲" prop="SongInfo.Name"> </el-table-column>
-      <el-table-column label="難度" prop="SongInfo.Lv"> </el-table-column>
+    <el-table border :data="SongInfo" max-height="960">
+      <el-table-column label="楽曲" prop="Name"> </el-table-column>
+      <el-table-column label="難度" prop="Lv"> </el-table-column>
       <el-table-column label="総アピール値">
         <template v-slot="_">
           <span>{{ appealValue }}</span>
@@ -75,111 +75,43 @@
       </el-table-column>
     </el-table>
 
-    <el-table border :data="simuResult" max-height="960">
-      <el-table-column label="最大" prop="ScoreInfo.Total.Ideal">
+    <el-table border :data="Total" max-height="960">
+      <el-table-column label="最大" prop="Ideal">
       </el-table-column>
-      <el-table-column label="0.1%" prop="ScoreInfo.Total.p[0]">
+      <el-table-column label="0.1%" prop="p[0]">
       </el-table-column>
-      <el-table-column label="1%" prop="ScoreInfo.Total.p[1]">
+      <el-table-column label="1%" prop="p[1]">
       </el-table-column>
-      <el-table-column label="50%" prop="ScoreInfo.Total.p[2]">
+      <el-table-column label="50%" prop="p[2]">
       </el-table-column>
     </el-table>
 
-    <el-table border :data="simuResult" max-height="960">
-      <el-table-column label="バースト前 最大" prop="ScoreInfo.1stHalf.Ideal">
+    <el-table border :data="FirstHalf" max-height="960">
+      <el-table-column label="バースト前 最大" prop="Ideal">
       </el-table-column>
-      <el-table-column label="0.1%" prop="ScoreInfo.1stHalf.p[0]">
+      <el-table-column label="0.1%" prop="p[0]">
       </el-table-column>
-      <el-table-column label="1%" prop="ScoreInfo.1stHalf.p[1]">
+      <el-table-column label="1%" prop="p[1]">
       </el-table-column>
-      <el-table-column label="50%" prop="ScoreInfo.1stHalf.p[2]">
+      <el-table-column label="50%" prop="p[2]">
       </el-table-column>
     </el-table>
 
     <el-collapse>
       <el-collapse-item title="レポートの詳細">
-        <el-table :data="simuResult">
-          <el-table-column label="カード名" prop="UnitInfo.Main1.Name">
+        <el-table :data="Mains">
+          <el-table-column label="カード名" prop="Main.Name">
           </el-table-column>
           <el-table-column
             label="スキル"
-            prop="UnitInfo.Main1.SkillEffectDescription"
+            prop="Main.SkillEffectDescription"
           >
           </el-table-column>
-          <el-table-column label="最大回数" prop="UnitInfo.Main1.MaxActNum">
+          <el-table-column label="最大回数" prop="Main.MaxActNum">
           </el-table-column>
           <el-table-column
             label="発動総数"
-            prop="UnitInfo.Main1.TotalSimulatedActNum"
-          >
-          </el-table-column>
-        </el-table>
-
-        <el-table :data="simuResult">
-          <el-table-column label="カード名" prop="UnitInfo.Main2.Name">
-          </el-table-column>
-          <el-table-column
-            label="スキル"
-            prop="UnitInfo.Main2.SkillEffectDescription"
-          >
-          </el-table-column>
-          <el-table-column label="最大回数" prop="UnitInfo.Main2.MaxActNum">
-          </el-table-column>
-          <el-table-column
-            label="発動総数"
-            prop="UnitInfo.Main2.TotalSimulatedActNum"
-          >
-          </el-table-column>
-        </el-table>
-
-        <el-table :data="simuResult">
-          <el-table-column label="カード名" prop="UnitInfo.Main3.Name">
-          </el-table-column>
-          <el-table-column
-            label="スキル"
-            prop="UnitInfo.Main3.SkillEffectDescription"
-          >
-          </el-table-column>
-          <el-table-column label="最大回数" prop="UnitInfo.Main3.MaxActNum">
-          </el-table-column>
-          <el-table-column
-            label="発動総数"
-            prop="UnitInfo.Main3.TotalSimulatedActNum"
-          >
-          </el-table-column>
-        </el-table>
-
-        <el-table :data="simuResult">
-          <el-table-column label="カード名" prop="UnitInfo.Main4.Name">
-          </el-table-column>
-          <el-table-column
-            label="スキル"
-            prop="UnitInfo.Main4.SkillEffectDescription"
-          >
-          </el-table-column>
-          <el-table-column label="最大回数" prop="UnitInfo.Main4.MaxActNum">
-          </el-table-column>
-          <el-table-column
-            label="発動総数"
-            prop="UnitInfo.Main4.TotalSimulatedActNum"
-          >
-          </el-table-column>
-        </el-table>
-
-        <el-table :data="simuResult">
-          <el-table-column label="カード名" prop="UnitInfo.Main5.Name">
-          </el-table-column>
-          <el-table-column
-            label="スキル"
-            prop="UnitInfo.Main5.SkillEffectDescription"
-          >
-          </el-table-column>
-          <el-table-column label="最大回数" prop="UnitInfo.Main5.MaxActNum">
-          </el-table-column>
-          <el-table-column
-            label="発動総数"
-            prop="UnitInfo.Main5.TotalSimulatedActNum"
+            prop="Main.TotalSimulatedActNum"
           >
           </el-table-column>
         </el-table>
@@ -205,6 +137,29 @@ export default {
       type: Array,
       required: true,
       default: []
+    }
+  },
+  computed: {
+    SongInfo() {
+      return [this.simuResult[0].SongInfo]
+    },
+    Mains() {
+      const UnitInfo = this.simuResult[0].UnitInfo
+      const values = Object.values(UnitInfo)
+      const mains = values.map(v => {
+        return {
+          'Main': v
+        }
+      })
+
+      mains.shift()
+      return mains
+    },
+    FirstHalf() {
+      return [this.simuResult[0].ScoreInfo['1stHalf']]
+    },
+    Total() {
+      return [this.simuResult[0].ScoreInfo['Total']]
     }
   }
 }
